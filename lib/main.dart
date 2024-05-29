bc60b5efd71a4ac2ac452fe19981f0ec2c1021d2
Billme Cart <mnrunais@gmail.com>
2024-05-29 17:15:00 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/view/home/screen_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const ScreenHome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
